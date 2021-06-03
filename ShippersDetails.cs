195b31582e3b990e776013107bfc75455961cba7
Lokesh Raj <lokeshraja84@gmail.com>
2021-06-03 16:02:27 +0530

    add ShipperClass

---
+++ ShippersDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiWithEFDemo
+{
+    public class ShippersDetails
+    {
+        public int Shipperid { get; set; }
+        public String Companyname { get; set; }
+        public String Phone { get; set; }
+    }
+}
